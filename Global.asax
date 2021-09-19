4d4f7ec7a0a67e145b0fd02e93b3825bbd84e6ea
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-09-19 20:33:01 +0600

    This

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LogInInfoDetails.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogInInfoDetails.Startup))]
+namespace LogInInfoDetails
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
