44bbb869aee39a81e7559308b50eaa83829b447f
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-09-22 17:47:30 +0600

    New Project With Individual Authentication Added

---
+++ AuthSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthSystem.Startup))]
+namespace AuthSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
